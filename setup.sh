d22a3857e3caffb9bb4d93e365d507b51929e134
Aaron N. Tubbs <atubbs@circumlocution.org>
2013-09-28 13:14:00 -0400

    Some changes.

--- setup.sh
+++ setup.sh
@@ -20,7 +20,22 @@ for file in vimrc vim zshrc zsh tmux.conf gitconfig; do
       fi
     fi
     # Add the appropriate symlink
-    echo "Symlinking ~/dotfiles/${file} to ~/.${file}"
+    echo "Symlinking ~/code/dotfiles/${file} to ~/.${file}"
     ln -s ~/code/dotfiles/${file} ~/.${file}
+  fi
+done
+
+# non-dot things; same as above otherwise
+for file in scripts; do
+  if [ ! -h ~/${file} ]; then
+    if [ -e ~/${file} ]; then
+      read -p "Move existing $file to ${file}_old? y[n] " -n 1
+      echo
+      if [[ $REPLY =~ ^[Yy]$ ]]; then
+        mv ~/${file} ~/${file}_old
+      fi
+    fi
+    echo "Symlinking ~/code/dotfiles/${file} to ~/${file}"
+    ln -s ~/code/dotfiles/${file} ~/${file}
   fi
 done
