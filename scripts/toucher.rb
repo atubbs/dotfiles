57ce5b7959acaac0bf1f280b075e42d78b067da0
Aaron N. Tubbs <atubbs@server.fake>
2013-10-02 23:29:02 -0400

    A dumb thing to touch files every so often.

---
+++ scripts/toucher.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/ruby
+
+require 'yaml'
+
+# ~/.toucher.yaml:
+# --- # files to touch
+# - /Volumes/Backup/.toucher
+# - /Volumes/Left/.toucher
+
+CONFIGFILE = ENV['HOME'] + '/.toucher.yaml'
+if not File.exists?(CONFIGFILE)
+  exit
+end
+File.open(CONFIGFILE) { |yf| YAML::load(yf) }.each { |touchfile| File.open(touchfile, 'w') { |file| file.write(Time.now.getutc) } }
+
+#cnf = YAML::load_file(File.open(ENV['HOME']+'/.toucher.yaml'))
